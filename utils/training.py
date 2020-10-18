f95cca9efa0c4c3462d4e6bb84c219b659acbf75
saswatpp <saswat.papp43@gmail.com>
2020-10-18 20:50:09 +0530

    setupmodules

---
+++ utils/training.py
@@ -0,0 +1,56 @@
+import torch
+import numpy
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from tqdm import tqdm
+
+def train_CapsNet(model, optimizer, train_loader, epoch, args):
+    capsule_net = model
+    capsule_net.train()
+    n_batch = len(train_loader)
+    total_loss = 0
+    for batch_id, (data, target) in enumerate(tqdm(train_loader)):
+        target = torch.sparse.torch.eye(10).index_select(dim=0, index=target)
+        if(args['USE_CUDA']):
+            data, target = data.cuda(), target.cuda()
+
+        optimizer.zero_grad()
+        output, reconstructions, masked = capsule_net(data)
+        loss = capsule_net.loss(data, output, target, reconstructions)
+        loss.backward()
+        optimizer.step()
+        correct = torch.sum(torch.argmax(masked, 1) == torch.argmax(target, 1))
+        train_loss = loss.item()
+        total_loss += train_loss
+        if batch_id % 100 == 0:
+            tqdm.write("Epoch: [{}/{}], Batch: [{}/{}], train accuracy: {:.6f}, loss: {:.6f}".format(
+                epoch,
+                args['N_EPOCHS'],
+                batch_id + 1,
+                n_batch,
+                correct / float(args['BATCH_SIZE']),
+                train_loss / float(args['BATCH_SIZE'])
+                ))
+    tqdm.write('Epoch: [{}/{}], train loss: {:.6f}'.format(epoch,args['N_EPOCHS'],total_loss / len(train_loader.dataset)))
+    
+def test_CapsNet(capsule_net, test_loader, epoch, args):
+    capsule_net.eval()
+    test_loss = 0
+    correct = 0
+    for batch_id, (data, target) in enumerate(test_loader):
+
+        target = torch.sparse.torch.eye(10).index_select(dim=0, index=target)
+        if args['USE_CUDA']:
+            data, target = data.cuda(), target.cuda()
+
+        output, reconstructions, masked = capsule_net(data)
+        loss = capsule_net.loss(data, output, target, reconstructions)
+
+        test_loss += loss.item()
+        correct += torch.sum(torch.argmax(masked, 1) == torch.argmax(target, 1))
+
+    tqdm.write(
+        "Epoch: [{}/{}], test accuracy: {:.6f}, loss: {:.6f}".format(epoch, args['N_EPOCHS'], correct / len(test_loader.dataset),
+                                                                  test_loss / len(test_loader)))
+
